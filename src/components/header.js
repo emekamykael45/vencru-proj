db2335a59ce03ebfda88bd05bfabe5e6b961f819
emekamykael45 <emekamykael45@gmail.com>
2023-01-06 19:28:52 +0100

    fix: sidebar toggler onclick

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,7 @@ import Icon from "../assets/svg";
 
 const Header = ({ open, toggle }) => {
   return (
-    <div
-      className="flex justify-between items-center p-[15px_16px] md:p-[0px_24px] w-full h-[60px] md:h-auto bg-white"
-      onClick={() => toggle()}
-    >
+    <div className="flex justify-between items-center p-[15px_16px] md:p-[0px_24px] w-full h-[60px] md:h-auto bg-white">
       <div className="flex items-center">
         <Logo />
         <p className="font-medium text-[20px] pl-[10px]">Untitled UI</p>
@@ -16,11 +13,11 @@ const Header = ({ open, toggle }) => {
       {window.innerWidth < 769 && (
         <>
           {open ? (
-            <div className="close_menu rotate-[45deg]">
+            <div className="close_menu rotate-[45deg]" onClick={() => toggle()}>
               <Icon name="plus" />
             </div>
           ) : (
-            <div className="rotate-[270deg]">
+            <div className="rotate-[270deg]" onClick={() => toggle()}>
               <Icon name="dashboard" />
             </div>
           )}
